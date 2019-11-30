cc739edca45df96919e3ebc73f1666de320a975d
Chupachis <45467849+Chupachis@users.noreply.github.com>
2019-11-30 04:20:26 +0100

    Update twitterbot_textfile.py

--- twitterbot/twitterbot_textfile.py
+++ twitterbot/twitterbot_textfile.py
@@ -1,1 +1,9 @@
+from os import environ
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+access_key = environ['ACCESS_KEY']
+access_secret = environ['ACCESS_SECRET']
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
