59bbdf5fe8a822d7f5390fa00037c12f55890298
Chupachis <45467849+Chupachis@users.noreply.github.com>
2020-02-06 23:35:12 +0100

    Update twitterbot_textfile.py

--- twitterbot_textfile.py
+++ twitterbot_textfile.py
@@ -5,6 +5,7 @@ import os
 
 from time import sleep
 from credentials import *
+from random import randint
 
 #from os import environ
 #consumer_key = environ['CONSUMER_KEY']
@@ -16,39 +17,12 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-key = 'line'
-r = redis.from_url(os.environ.get("REDIS_URL"))
-line_index = r.get(key)
-if not line_index:
-    line_index = 0
-
-line_index = int(line_index)
-
-
-# Open text file endlessdisco.txt for reading
 my_file = open('endlessdisco.txt', 'r')
-
-# Read lines one by one from endlessdisco.txt and assign to file_lines variable
 file_lines = my_file.readlines()
+line = file_lines[randint(0, len(file_lines) - 1)]
+print(line)
 
-# Close file
-my_file.close()
-
-# Check if there are still lines to write
-if line_index + 1 > len(file_lines):
-    sys.exit("Not enought lines")
-
-# Get the right line to send
-line = file_lines[line_index]
-
-# Try to send the line to Twitter
 try:
-    print(line)
-    if line != '\n':
-        api.update_status(line)
-    else:
-        pass
+    api.update_status(line)
 except tweepy.TweepError as e:
     print(e.reason)
-
-r.set(key, line_index + 1) # Write the index to the DB
